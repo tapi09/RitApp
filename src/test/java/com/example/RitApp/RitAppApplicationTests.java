cea83f8cf494621253b424c24bef88d15a0b8650
Agustin Sochi <sochi10agu@gmail.com>
2021-10-27 20:26:23 -0300

    Commit de Todo el Proyecto

---
+++ src/test/java/com/example/RitApp/RitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RitApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
