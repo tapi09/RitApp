f8224583a07299c9cc05a9baf6c2c5fb20cda81a
David Sanchez <tapi.cr7@gmail.com>
2021-10-28 13:15:57 -0300

    packages

---
+++ src/main/java/com/RitApp/web/RitAppApplication.java
@@ -0,0 +1,13 @@
+package com.RitApp.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RitAppApplication.class, args);
+	}
+
+}
